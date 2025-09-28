3a7be3a59f6fc82c2f2baf4ca58b0d8ac7a3fb03
aliffikri21 <haykalalif21@gmail.com>
2025-09-28 21:27:23 +0800

    update 2

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,13 @@ Route::get('/dashboard', function () {
     ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    // CRUD MAHASISWA
-    Route::get('/mahasiswa', [MahasiswaController::class, 'index']); // Menampilkan view
-    Route::get('/mahasiswa/create', [MahasiswaController::class, 'create']); // Menampilkan view create
-    Route::post('/mahasiswa/store', [MahasiswaController::class, 'store']); // Proses tambah data mahasiswa
-    Route::get('/mahasiswa/{mahasiswa}/edit', [MahasiswaController::class, 'edit']); // menampilkan view edit
-    Route::put('/mahasiswa/{mahasiswa}/update', [MahasiswaController::class, 'update']); // proses update data mahasiswa
-    Route::delete('/mahasiswa/{mahasiswa}', [MahasiswaController::class, 'destroy']); //proses hapus data mahasiswa
 
+    Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+    Route::get('/mahasiswa/create', [MahasiswaController::class, 'create']);
+    Route::post('/mahasiswa/store', [MahasiswaController::class, 'store']);
+    Route::get('/mahasiswa/{mahasiswa}/edit', [MahasiswaController::class, 'edit']);
+    Route::put('/mahasiswa/{mahasiswa}/update', [MahasiswaController::class, 'update']);
+    Route::delete('/mahasiswa/{mahasiswa}', [MahasiswaController::class, 'destroy']);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
